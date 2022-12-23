a745c65b3ca79f171c7d3dae9aee1f40d1c25f2b
dvpesh <dvpesh@mail.ru>
2022-12-23 23:22:06 +0300

    реализован паттерн Proxy для CatergoryService с кэшем на REDIS

--- homework6/core-service/src/main/java/ru/peshekhonov/core/services/CategoryService.java
+++ homework6/core-service/src/main/java/ru/peshekhonov/core/services/CategoryService.java
@@ -1,6 +1,8 @@
 package ru.peshekhonov.core.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.peshekhonov.core.entities.Category;
 import ru.peshekhonov.core.repositories.CategoryRepository;
@@ -10,14 +12,17 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "categoryCache")
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    @Cacheable(key = "#title")
     public Optional<Category> findByTitle(String title) {
         return categoryRepository.findByTitle(title);
     }
 
+    @Cacheable
     public List<String> findAllTitles() {
         return categoryRepository.findAll().stream().map(Category::getTitle).toList();
     }
