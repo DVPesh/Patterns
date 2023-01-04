d507ec940a79b071965ec819ad16a62d7858caf0
dvpesh <dvpesh@mail.ru>
2023-01-04 03:27:15 +0300

    Реализован паттерн Listener с использованием RabbitMQ. Когда менеджер добавляет новый продукт или удаляет существующий, то клиенты получают уведомления об этом в своих браузерах.

--- homework6/core-service/src/main/java/ru/peshekhonov/core/listener/CoreServiceEventListener.java
+++ homework6/core-service/src/main/java/ru/peshekhonov/core/listener/CoreServiceEventListener.java
@@ -2,9 +2,12 @@ package ru.peshekhonov.core.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import ru.peshekhonov.api.dto.ProductDto;
 import ru.peshekhonov.core.events.ProductCreatedEvent;
 import ru.peshekhonov.core.events.ProductDeletedEvent;
 import ru.peshekhonov.core.mappers.ProductDtoMapper;
@@ -15,16 +18,30 @@ import ru.peshekhonov.core.mappers.ProductDtoMapper;
 public class CoreServiceEventListener {
 
     private final ProductDtoMapper productDtoMapper;
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public void setRabbitTemplate(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     @Async
     @EventListener
     public void handleEvent(ProductCreatedEvent event) {
-        log.info("Добавлен новый товар: {}", productDtoMapper.map(event.getProduct()));
+        ProductDto productDto = productDtoMapper.map(event.getProduct());
+        log.info("Добавлен новый товар: {}", productDto);
+        rabbitTemplate.convertAndSend("STOMP", null,
+                String.format("В категории '%s' добавлен новый товар '%s' id=%s, цена %s", productDto.getCategoryTitle(),
+                        productDto.getTitle(), productDto.getId(), productDto.getPrice()));
     }
 
     @Async
     @EventListener
     public void handleEvent(ProductDeletedEvent event) {
-        log.info("Удален товар: {}", productDtoMapper.map(event.getProduct()));
+        ProductDto productDto = productDtoMapper.map(event.getProduct());
+        log.info("Удален товар: {}", productDto);
+        rabbitTemplate.convertAndSend("STOMP", null,
+                String.format("Из категории '%s' удалён товар '%s' id=%s, цена %s", productDto.getCategoryTitle(),
+                        productDto.getTitle(), productDto.getId(), productDto.getPrice()));
     }
 }
